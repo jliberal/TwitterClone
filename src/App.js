fa491ce1f759620eec9f7ff5e145c9c94a20ab04
John Paul Liberal <jliberal@gmail.com>
2020-08-08 01:50:42 -0400

    Sidebar & Material UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    //Convention BEM al CSS class names are lowercase
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Aquí va la App</h1>
+      {/* Sidebar */}
+
+      {/* Centerbar */}
+
+      {/* Widgetsbar */}
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Sidebar = () => {
+    return(
+        <div className="sidebar">
+            {/* Icon */}
+
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+
+            {/* Button */}
+        </div>
+    );
+}
+
+export default Sidebar;
